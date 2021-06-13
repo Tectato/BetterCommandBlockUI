4c673097656d384e3d3d8baf86c965b7e7ac2390
Tectato <lukasmoersler@gmail.com>
2021-06-13 21:25:17 +0200

    update to 1.17

--- src/main/java/vectorientation/mixin/FallingBlockRendererMixin.java
+++ src/main/java/vectorientation/mixin/FallingBlockRendererMixin.java
@@ -8,10 +8,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.FallingBlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
 import vectorientation.main.Vectorientation;
 
 @Mixin(FallingBlockEntityRenderer.class)
@@ -35,11 +35,11 @@ public class FallingBlockRendererMixin {
 			method = "render(Lnet/minecraft/entity/FallingBlockEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"
 			)
 	public void addRotation(FallingBlockEntity fallingBlockEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info) {
-		Vector3f vel = new Vector3f(fallingBlockEntity.getVelocity());
+		Vec3f vel = new Vec3f(fallingBlockEntity.getVelocity());
 		float speed = Vectorientation.squetch ? (float) Math.sqrt(vel.getX() * vel.getX() + vel.getY() * vel.getY() + vel.getZ() * vel.getZ()) : 0;
 		vel.normalize();
 		float angle = (float) Math.acos(vel.getY());
-		vel = new Vector3f(-1 * vel.getZ(), 0, vel.getX());
+		vel = new Vec3f(-1 * vel.getZ(), 0, vel.getX());
 		vel.normalize();
 		Quaternion rot = new Quaternion(vel, -angle, false);
 		matrixStack.translate(0.5D, 0.0D, 0.5D);
