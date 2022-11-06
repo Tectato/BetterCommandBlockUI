8648a4a6917a937c4fb40125110e1c2ae7e06e78
Tectato <lukasmoersler@gmail.com>
2022-11-06 22:04:24 +0100

    rebase onto main (contd. 2)

--- src/main/java/bettercommandblockui/main/BetterCommandBlockUI.java
+++ src/main/java/bettercommandblockui/main/BetterCommandBlockUI.java
@@ -6,7 +6,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
@@ -52,7 +52,7 @@ public class BetterCommandBlockUI implements ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (areaSelectionInput.wasPressed()) {
                 boolean startedSelection = AreaSelectionHandler.areaSelectionInput();
-                client.player.sendMessage(Text.literal(startedSelection ? "[Started Area Selection]" : "[Area Selector pasted to clipboard]"), true);
+                client.player.sendMessage(new LiteralText(startedSelection ? "[Started Area Selection]" : "[Area Selector pasted to clipboard]"), true);
             }
         });
 
--- src/main/java/bettercommandblockui/main/ui/ScrollbarWidget.java
+++ src/main/java/bettercommandblockui/main/ui/ScrollbarWidget.java
@@ -42,6 +42,7 @@ public class ScrollbarWidget extends ClickableWidget {
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         if (!this.visible) {
+            this.dragging = false;
             return;
         }
         this.hovered = checkHovered(mouseX, mouseY);
